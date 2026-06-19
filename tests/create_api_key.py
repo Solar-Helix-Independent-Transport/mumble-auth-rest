e7f2b3d8652360eff1e41a946fdbbd4087308b7e
Aaron Kable <aaronkable@gmail.com>
2026-06-19 22:11:27 +0800

    db updates

--- tests/create_api_key.py
+++ tests/create_api_key.py
@@ -7,6 +7,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mumble_rest.settings_docker")
 import django
 django.setup()
 
+from django.core.management import call_command
+call_command('migrate', verbosity=0)
+
 from mumble_rest.models import APIKey, generate_token
 
 k = generate_token()
