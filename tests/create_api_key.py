0a027600aff8b1e74a0ae698eab7d68f187f8585
Aaron Kable <aaronkable@gmail.com>
2026-06-20 12:25:04 +0800

    race conditions

--- tests/create_api_key.py
+++ tests/create_api_key.py
@@ -7,6 +7,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mumble_rest.settings_docker")
 import django
 django.setup()
 
+from django.core.management import call_command
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
+
+executor = MigrationExecutor(connection)
+if executor.migration_plan(executor.loader.graph.leaf_nodes()):
+    call_command('migrate', verbosity=0)
+
 from mumble_rest.models import APIKey, generate_token
 
 k = generate_token()
